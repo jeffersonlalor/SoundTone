d752199e8ea0f653117b9735de489a278a9eeb9a
Jefferson de Oliveira Lalor <jeffersonlalor19@gmail.com>
2018-11-16 10:46:34 -0400

    Aplicando iniciar do CoreData

---
+++ Is Loh/Model/ModelController.swift
@@ -0,0 +1,15 @@
+//
+//  ModelController.swift
+//  SoundTone
+//
+//  Created by Jefferson de Oliveira Lalor on 16/11/18.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import UIKit
+
+class ModelController {
+    
+
+    
+}
