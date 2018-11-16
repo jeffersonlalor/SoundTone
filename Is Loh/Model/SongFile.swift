9323998ff30f8d45c7fe166b98c924975eccfe42
Jefferson de Oliveira Lalor <jeffersonlalor19@gmail.com>
2018-11-16 10:55:26 -0400

    Criação do arquivo SongFlie

---
+++ Is Loh/Model/SongFile.swift
@@ -0,0 +1,13 @@
+//
+//  SongFileViewController.swift
+//  SoundTone
+//
+//  Created by Jefferson de Oliveira Lalor on 16/11/18.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import UIKit
+
+class SongFile {
+    
+}
